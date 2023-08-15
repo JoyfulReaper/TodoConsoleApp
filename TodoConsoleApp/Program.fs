09497a68ab3e73a393bfc6485a4389d867be44e2
Kyle Givler <xvegan88x@gmail.com>
2023-08-15 19:45:53 -0400

    Show validation errors

--- TodoConsoleApp/Program.fs
+++ TodoConsoleApp/Program.fs
@@ -7,7 +7,7 @@ open Validation
 let showHelp () =
     printfn "ConsoleTodoApp Help"
     printfn "Add Todo: /a [Title] [Description] [user]"
-    printfn "Edit Todo: /e [Id] [title] {description} [user]"
+    printfn "Edit Todo: /e [Id] [title] [description] [user]"
     printfn "Complete Todo: /m [Id]"
     printfn "Clear All Todos: /c"
     printfn "Show Todo: /s [Id]"
@@ -34,8 +34,8 @@ let main args =
         match todo with
         | Ok todo ->
             addTodo todo
-        | Error e ->
-            showHelp ()
+        | Error (ValidationError e) ->
+            printfn "Validation error: %s\nUse /h for help" e
     | CommandLineParser.Edit ->
         let todo = 
             commandLineOptions
@@ -46,8 +46,8 @@ let main args =
         match todo with
         | Ok todo ->
             editTodo todo
-        | Error _ ->
-            showHelp ()
+        | Error (ValidationError e) ->
+            printfn "Validation error: %s\nUse /h for help" e
     | CommandLineParser.MarkDone ->
         let todoId = 
             commandLineOptions.TodoId
@@ -57,8 +57,8 @@ let main args =
         match todoId with
         | Ok todoId ->
             markDone todoId
-        | Error _ ->
-            showHelp ()
+        | Error (ValidationError e) ->
+            printfn "Validation error: %s\nUse /h for help" e
 
     | CommandLineParser.ClearAll ->
         clearAll ()
@@ -71,8 +71,8 @@ let main args =
         match todoId with
         | Ok todoId ->
             show todoId
-        | Error _ ->
-            showHelp ()
+        | Error (ValidationError e) ->
+            printfn "Validation error: %s\nUse /h for help" e
     | CommandLineParser.ShowAll ->
         showAll()
     | CommandLineParser.Help ->
